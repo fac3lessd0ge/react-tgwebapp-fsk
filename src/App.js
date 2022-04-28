aeeebf4ee766bfd33fce42e77227122dff2b6ed4
fac3lessd0ge <garmasharseniy@gmail.com>
2022-04-28 20:16:04 +0300

    Composing first page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import * as React from 'react';
 import { useTelegramWebApp } from 'react-telegram-webapp';
 import './App.css';
 import Button from './components/Button';
-import { TelegramWebApp } from 'react-telegram-webapp';
 import GradientEllipse from './components/GradientEllipse/GradientEllipse';
+import RoundButton from './components/RoundButton/RoundButton';
+import TextContainer from './components/TextField/TextContainer';
 
 const App = () => {
 	const [message, setMessage] = React.useState('')
@@ -15,11 +16,27 @@ const App = () => {
 		webApp.sendData(message);
 	}
 
-	return ( 
-		<main className='App'>
-			<div className='content' value={message} onChange={(e) => {setMessage(e.target.value)} }>
-				<input placeholder='Введите имя' />
-				<Button innerText={'Press me!'} clickHandler={buttonClickHandler}/>
+	return (
+		<main className="App">
+			<div
+				className="content"
+				value={message}
+				onChange={(e) => {
+					setMessage(e.target.value);
+				}}
+			>
+				<TextContainer>
+					СКОЛЬКО КОМНАТ ДОЛЖНО БЫТЬ В <br /> КВАРТИРЕ?
+				</TextContainer>
+				<div className="grid-wrapper">
+					{['С', 1, 2, 3, 4, 5].map((element) => (
+						<RoundButton>{element}</RoundButton>
+					))}
+				</div>
+				<Button
+					innerText={'ВЫБРАТЬ'}
+					clickHandler={buttonClickHandler}
+				/>
 				{message}
 				<GradientEllipse />
 			</div>
