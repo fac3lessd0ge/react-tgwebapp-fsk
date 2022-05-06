90dac60fd7d2ab6e76d7c839281a1bfadb81b777
fac3lessd0ge <garmasharseniy@gmail.com>
2022-05-06 16:34:32 +0300

    double thumb

--- src/ThirdLayout.jsx
+++ src/ThirdLayout.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import TextContainer from './components/TextField/TextContainer';
 import StandartLayout from './StandartLayout';
 import Button from './components/Button/Button';
+import { TwoThumbInputRange } from 'react-two-thumb-input-range';
+import thumbs from './thumbs.svg'
 
 
 const style = {
@@ -13,12 +15,62 @@ const style = {
     width: '50%'
 }
 
+const thumbStyle = {
+    width: '2rem',
+    height: '2rem',
+    borderRadius: '50%',
+    backgroundImage: `url(${thumbs})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center'
+}
+
+const labelStyle = {
+    backgroundColor: '#14141400'
+}
+
+const inputStyle = {
+    width: '50vw',
+    height: '2px'
+}
+
 const ThirdLayout = () => {
+    const [value, setValue] = React.useState([7, 20])
+
+    const onValueChange = (values) => {
+        setValue(values);
+    }
+
     return (
         <StandartLayout>
             <TextContainer>
                 {'В каком ценовом диапазоне вы ищете квартиру?'.toUpperCase()}
             </TextContainer>
+            <br/>
+            <div 
+                style={
+                    {
+                        width: '50%',
+                        height: '10rem',
+                        background: 'url(thumbs.svg)'
+                    }
+                }
+            >
+
+            </div>
+            <TwoThumbInputRange 
+                onChange={onValueChange}
+                values={value}
+                min={0}
+                max={50}
+                railColor='#727272'
+                trackColor='#E84E0E'
+                thumbColor='#E84E0E'
+                showLabels={true}
+                thumbStyle={thumbStyle}
+                inputStyle={inputStyle}
+                labelStyle={labelStyle}
+            />
+            <br/>
             <Button innerText={'Подтвердить'} linkToPath=''/>
             
         </StandartLayout>
