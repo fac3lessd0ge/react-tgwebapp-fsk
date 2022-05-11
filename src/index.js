e1aac33fa0e7abbe051a581db22776dd91978c85
fac3lessd0ge <garmasharseniy@gmail.com>
2022-05-11 20:36:57 +0300

    start page config

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Layouts/App';
 import { TelegramWebApp } from 'react-telegram-webapp';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './fonts/FuturaFuturis/style.css';
 import SecondLayout from './Layouts/SecondLayout';
 import ThirdLayout from './Layouts/ThirdLayout';
@@ -19,12 +19,26 @@ async function validateHash(hash) {
 	return response.ok;
 }
 
+const loggedIn = { page : 4 } // [0, 1, 2, 3, 4, 5]
+
+const linksTo = {
+	1: '/testreactjs/', 
+	2: '/testreactjs/second',
+	3: '/testreactjs/third',
+	4: '/testreactjs/fourth',
+	5: '/testreactjs/fifth'
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<TelegramWebApp validateHash={validateHash}>
 		<BrowserRouter>
 			<Routes>
-				<Route path="/testreactjs/" element={<App />} />
+				<Route 
+					exact 
+					path="/testreactjs/" 
+					element={loggedIn.current ? <Navigate replace to={linksTo[loggedIn.page]}/> : <App />} 
+				/>
 				<Route path='/testreactjs/second' element={ <SecondLayout /> } />
 				<Route path='/testreactjs/third' element={<ThirdLayout />} />
 				<Route path='/testreactjs/fourth' element={<FourthLayout />} />
