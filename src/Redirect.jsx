84c953e1a08d43a308e2c183c1258001303a0891
fac3lessd0ge <garmasharseniy@gmail.com>
2022-05-13 01:51:10 +0300

    currentPage+1 fix

--- src/Redirect.jsx
+++ src/Redirect.jsx
@@ -32,7 +32,7 @@ const Redirect = () => {
     }, [initData])
     return (
         <>
-            {(currentPage !== null) && <Navigate replace to={linksTo[currentPage]}/>}
+            {(currentPage !== null) && <Navigate replace to={linksTo[currentPage + 1]}/>}
         </>  
     );
 }
